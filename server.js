15da44627665bd270ed53e6f60c0daaa02257dcc
ciocan <ciocan@gmail.com>
2018-01-06 21:46:53 +0000

    remove unused deps

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const next = require('next');
 const routes = require('./routes');
+const express = require('express');
+
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
-const express = require('express');
 app.prepare().then(() => {
   express().use(handler).listen(3000);
 });
